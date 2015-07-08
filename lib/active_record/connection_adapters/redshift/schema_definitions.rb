56f81ab6976a9641068fcf372baf5b6c84ecc389
Yura Taras <yura.taras@gmail.com>
2015-07-08 13:38:34 +0300

    Update schema_definitions.rb

--- lib/active_record/connection_adapters/redshift/schema_definitions.rb
+++ lib/active_record/connection_adapters/redshift/schema_definitions.rb
@@ -54,7 +54,7 @@ module ActiveRecord
 
         def new_column_definition(name, type, options) # :nodoc:
           column = super
-          column.array = options[:array]
+          # column.array = options[:array]
           column
         end
 
