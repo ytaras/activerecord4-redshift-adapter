4f0c24d0f3378f2682da67deb7daeb489ebfcd12
Yura Taras <yura.taras@gmail.com>
2015-07-08 13:49:15 +0300

    Update redshift_adapter.rb

--- lib/foreigner/connection_adapters/redshift_adapter.rb
+++ lib/foreigner/connection_adapters/redshift_adapter.rb
@@ -1,7 +1,9 @@
+require 'foreigner/connection_adapters/postgresql_adapter'
 module Foreigner
   module ConnectionAdapters
     module RedshiftAdapter
       include Foreigner::ConnectionAdapters::PostgreSQLAdapter
+      
     end
   end
 end
